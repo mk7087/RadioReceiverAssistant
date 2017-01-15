9ca04897661068d61928900a3653ce67bb45f102
mk7087 <yamk4054@outlook.com>
2017-01-15 12:40:18 +0900

    Updated Form

--- Form.Designer.vb
+++ Form.Designer.vb
@@ -22,43 +22,46 @@ Partial Class Form
   'コード エディターを使って変更しないでください。
   <System.Diagnostics.DebuggerStepThrough()>
   Private Sub InitializeComponent()
-    Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
-    Me.ToolStripStatusLabel1 = New System.Windows.Forms.ToolStripStatusLabel()
+    Me.StatusStrip = New System.Windows.Forms.StatusStrip()
+    Me.ToolStripStatusLabel = New System.Windows.Forms.ToolStripStatusLabel()
     Me.ToolStripProgressBar1 = New System.Windows.Forms.ToolStripProgressBar()
     Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
     Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
-    Me.TextBox2 = New System.Windows.Forms.TextBox()
-    Me.SearchLabel2 = New System.Windows.Forms.Label()
+    Me.TypeSearchBox = New System.Windows.Forms.TextBox()
+    Me.TypeSearchLabel = New System.Windows.Forms.Label()
     Me.DataGridView1 = New System.Windows.Forms.DataGridView()
     Me.SearchLabel = New System.Windows.Forms.Label()
     Me.SearchBox = New System.Windows.Forms.TextBox()
-    Me.ComboBox1 = New System.Windows.Forms.ComboBox()
-    Me.StatusStrip1.SuspendLayout()
+    Me.SearchTypeBox = New System.Windows.Forms.ComboBox()
+    Me.StatusStrip.SuspendLayout()
     Me.TableLayoutPanel1.SuspendLayout()
     CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
     Me.SuspendLayout()
     '
-    'StatusStrip1
+    'StatusStrip
     '
-    Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripStatusLabel1, Me.ToolStripProgressBar1})
-    Me.StatusStrip1.Location = New System.Drawing.Point(0, 504)
-    Me.StatusStrip1.Name = "StatusStrip1"
-    Me.StatusStrip1.Padding = New System.Windows.Forms.Padding(1, 0, 19, 0)
-    Me.StatusStrip1.Size = New System.Drawing.Size(918, 22)
-    Me.StatusStrip1.TabIndex = 0
-    Me.StatusStrip1.Text = "StatusStrip1"
+    Me.StatusStrip.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripStatusLabel, Me.ToolStripProgressBar1})
+    Me.StatusStrip.Location = New System.Drawing.Point(0, 504)
+    Me.StatusStrip.Name = "StatusStrip"
+    Me.StatusStrip.Padding = New System.Windows.Forms.Padding(1, 0, 19, 0)
+    Me.StatusStrip.Size = New System.Drawing.Size(918, 22)
+    Me.StatusStrip.TabIndex = 0
+    Me.StatusStrip.Text = "StatusStrip1"
     '
-    'ToolStripStatusLabel1
+    'ToolStripStatusLabel
     '
-    Me.ToolStripStatusLabel1.Name = "ToolStripStatusLabel1"
-    Me.ToolStripStatusLabel1.Size = New System.Drawing.Size(119, 17)
-    Me.ToolStripStatusLabel1.Text = "ToolStripStatusLabel1"
-    Me.ToolStripStatusLabel1.Visible = False
+    Me.ToolStripStatusLabel.Name = "ToolStripStatusLabel"
+    Me.ToolStripStatusLabel.Size = New System.Drawing.Size(765, 17)
+    Me.ToolStripStatusLabel.Spring = True
+    Me.ToolStripStatusLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+    Me.ToolStripStatusLabel.Visible = False
     '
     'ToolStripProgressBar1
     '
     Me.ToolStripProgressBar1.Name = "ToolStripProgressBar1"
-    Me.ToolStripProgressBar1.Size = New System.Drawing.Size(100, 16)
+    Me.ToolStripProgressBar1.Size = New System.Drawing.Size(200, 16)
+    Me.ToolStripProgressBar1.Step = 1
+    Me.ToolStripProgressBar1.Style = System.Windows.Forms.ProgressBarStyle.Marquee
     Me.ToolStripProgressBar1.Visible = False
     '
     'MenuStrip1
@@ -66,7 +69,7 @@ Partial Class Form
     Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
     Me.MenuStrip1.Name = "MenuStrip1"
     Me.MenuStrip1.Padding = New System.Windows.Forms.Padding(8, 3, 0, 3)
-    Me.MenuStrip1.Size = New System.Drawing.Size(1080, 25)
+    Me.MenuStrip1.Size = New System.Drawing.Size(918, 25)
     Me.MenuStrip1.TabIndex = 1
     Me.MenuStrip1.Text = "MenuStrip1"
     Me.MenuStrip1.Visible = False
@@ -76,46 +79,46 @@ Partial Class Form
     Me.TableLayoutPanel1.BackColor = System.Drawing.SystemColors.ControlLight
     Me.TableLayoutPanel1.ColumnCount = 5
     Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 96.0!))
-    Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
-    Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 53.0!))
-    Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
-    Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 27.0!))
-    Me.TableLayoutPanel1.Controls.Add(Me.TextBox2, 4, 0)
-    Me.TableLayoutPanel1.Controls.Add(Me.SearchLabel2, 2, 0)
+    Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 80.0!))
+    Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 96.0!))
+    Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10.0!))
+    Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10.0!))
+    Me.TableLayoutPanel1.Controls.Add(Me.TypeSearchBox, 4, 0)
+    Me.TableLayoutPanel1.Controls.Add(Me.TypeSearchLabel, 2, 0)
     Me.TableLayoutPanel1.Controls.Add(Me.DataGridView1, 0, 1)
     Me.TableLayoutPanel1.Controls.Add(Me.SearchLabel, 0, 0)
     Me.TableLayoutPanel1.Controls.Add(Me.SearchBox, 1, 0)
-    Me.TableLayoutPanel1.Controls.Add(Me.ComboBox1, 3, 0)
+    Me.TableLayoutPanel1.Controls.Add(Me.SearchTypeBox, 3, 0)
     Me.TableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill
-    Me.TableLayoutPanel1.Location = New System.Drawing.Point(0, 0)
+    Me.TableLayoutPanel1.Location = New System.Drawing.Point(0, 25)
     Me.TableLayoutPanel1.Margin = New System.Windows.Forms.Padding(0)
     Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
     Me.TableLayoutPanel1.RowCount = 2
     Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 42.0!))
     Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
-    Me.TableLayoutPanel1.Size = New System.Drawing.Size(918, 504)
+    Me.TableLayoutPanel1.Size = New System.Drawing.Size(918, 479)
     Me.TableLayoutPanel1.TabIndex = 2
     '
-    'TextBox2
+    'TypeSearchBox
     '
-    Me.TextBox2.Dock = System.Windows.Forms.DockStyle.Fill
-    Me.TextBox2.Location = New System.Drawing.Point(895, 5)
-    Me.TextBox2.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
-    Me.TextBox2.Name = "TextBox2"
-    Me.TextBox2.Size = New System.Drawing.Size(19, 27)
-    Me.TextBox2.TabIndex = 6
+    Me.TypeSearchBox.Dock = System.Windows.Forms.DockStyle.Fill
+    Me.TypeSearchBox.Location = New System.Drawing.Point(848, 5)
+    Me.TypeSearchBox.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+    Me.TypeSearchBox.Name = "TypeSearchBox"
+    Me.TypeSearchBox.Size = New System.Drawing.Size(66, 27)
+    Me.TypeSearchBox.TabIndex = 6
     '
-    'SearchLabel2
+    'TypeSearchLabel
     '
-    Me.SearchLabel2.AutoSize = True
-    Me.SearchLabel2.Dock = System.Windows.Forms.DockStyle.Fill
-    Me.SearchLabel2.Location = New System.Drawing.Point(471, 5)
-    Me.SearchLabel2.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
-    Me.SearchLabel2.Name = "SearchLabel2"
-    Me.SearchLabel2.Size = New System.Drawing.Size(45, 32)
-    Me.SearchLabel2.TabIndex = 5
-    Me.SearchLabel2.Text = "種類別検索"
-    Me.SearchLabel2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+    Me.TypeSearchLabel.AutoSize = True
+    Me.TypeSearchLabel.Dock = System.Windows.Forms.DockStyle.Fill
+    Me.TypeSearchLabel.Location = New System.Drawing.Point(680, 5)
+    Me.TypeSearchLabel.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+    Me.TypeSearchLabel.Name = "TypeSearchLabel"
+    Me.TypeSearchLabel.Size = New System.Drawing.Size(88, 32)
+    Me.TypeSearchLabel.TabIndex = 5
+    Me.TypeSearchLabel.Text = "種類別検索"
+    Me.TypeSearchLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
     '
     'DataGridView1
     '
@@ -129,7 +132,7 @@ Partial Class Form
     Me.DataGridView1.Name = "DataGridView1"
     Me.DataGridView1.ReadOnly = True
     Me.DataGridView1.RowTemplate.Height = 21
-    Me.DataGridView1.Size = New System.Drawing.Size(918, 462)
+    Me.DataGridView1.Size = New System.Drawing.Size(918, 437)
     Me.DataGridView1.TabIndex = 0
     '
     'SearchLabel
@@ -150,18 +153,18 @@ Partial Class Form
     Me.SearchBox.Location = New System.Drawing.Point(100, 5)
     Me.SearchBox.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
     Me.SearchBox.Name = "SearchBox"
-    Me.SearchBox.Size = New System.Drawing.Size(363, 27)
+    Me.SearchBox.Size = New System.Drawing.Size(572, 27)
     Me.SearchBox.TabIndex = 2
     '
-    'ComboBox1
+    'SearchTypeBox
     '
-    Me.ComboBox1.Dock = System.Windows.Forms.DockStyle.Fill
-    Me.ComboBox1.FormattingEnabled = True
-    Me.ComboBox1.Location = New System.Drawing.Point(524, 5)
-    Me.ComboBox1.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
-    Me.ComboBox1.Name = "ComboBox1"
-    Me.ComboBox1.Size = New System.Drawing.Size(363, 28)
-    Me.ComboBox1.TabIndex = 4
+    Me.SearchTypeBox.Dock = System.Windows.Forms.DockStyle.Fill
+    Me.SearchTypeBox.FormattingEnabled = True
+    Me.SearchTypeBox.Location = New System.Drawing.Point(776, 5)
+    Me.SearchTypeBox.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+    Me.SearchTypeBox.Name = "SearchTypeBox"
+    Me.SearchTypeBox.Size = New System.Drawing.Size(64, 28)
+    Me.SearchTypeBox.TabIndex = 4
     '
     'Form
     '
@@ -170,15 +173,15 @@ Partial Class Form
     Me.BackColor = System.Drawing.SystemColors.ControlLight
     Me.ClientSize = New System.Drawing.Size(918, 526)
     Me.Controls.Add(Me.TableLayoutPanel1)
-    Me.Controls.Add(Me.StatusStrip1)
+    Me.Controls.Add(Me.StatusStrip)
     Me.Controls.Add(Me.MenuStrip1)
     Me.Font = New System.Drawing.Font("メイリオ", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
     Me.MainMenuStrip = Me.MenuStrip1
     Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
     Me.Name = "Form"
     Me.Text = "Form1"
-    Me.StatusStrip1.ResumeLayout(False)
-    Me.StatusStrip1.PerformLayout()
+    Me.StatusStrip.ResumeLayout(False)
+    Me.StatusStrip.PerformLayout()
     Me.TableLayoutPanel1.ResumeLayout(False)
     Me.TableLayoutPanel1.PerformLayout()
     CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
@@ -187,15 +190,15 @@ Partial Class Form
 
   End Sub
 
-  Friend WithEvents StatusStrip1 As StatusStrip
+  Friend WithEvents StatusStrip As StatusStrip
   Friend WithEvents MenuStrip1 As MenuStrip
   Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
   Friend WithEvents DataGridView1 As DataGridView
-  Friend WithEvents TextBox2 As TextBox
-  Friend WithEvents SearchLabel2 As Label
+  Friend WithEvents TypeSearchBox As TextBox
+  Friend WithEvents TypeSearchLabel As Label
   Friend WithEvents SearchLabel As Label
   Friend WithEvents SearchBox As TextBox
-  Friend WithEvents ComboBox1 As ComboBox
-  Friend WithEvents ToolStripStatusLabel1 As ToolStripStatusLabel
+  Friend WithEvents SearchTypeBox As ComboBox
+  Friend WithEvents ToolStripStatusLabel As ToolStripStatusLabel
   Friend WithEvents ToolStripProgressBar1 As ToolStripProgressBar
 End Class
--- Form.vb
+++ Form.vb
@@ -1,5 +1,6 @@
 ﻿Public Class Form
   Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
     Text = My.Application.Info.Title
+    TableLayoutPanel1.RowStyles(0) = New RowStyle(SizeType.Absolute, Me.SearchTypeBox.Height + 6)
   End Sub
 End Class
